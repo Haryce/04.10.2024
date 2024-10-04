d49e9b9821160f6008dae4e7b54ce42738b4c512
Haryce <148765923+Haryce@users.noreply.github.com>
2024-10-04 08:37:22 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+const double PI = 3.14; //добавляем число пи
+using namespace std;
+//вычисление площади
+double area(double diameter) {  
+    double radius = diameter / 2.0;
+    return PI * pow(radius, 2);
+}
+ //вычисление перимитра
+double perimeter(double diameter) {
+    return PI * diameter;
+}
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double diameter;
+    int choice; 
+    cout << "Введите диаметр окружности: ";
+    cin >> diameter;
+    cout << "Выберите действие: \n1. Посчитать площадь\n2. Посчитать периметр\n";
+    //выбор действия пользователем
+    cin >> choice;
+    if (choice == 1) {
+        double area1 = area(diameter);
+        cout << "Площадь окружности: " << area1;
+    }
+    else if (choice == 2) {
+        double perimeter1 = perimeter(diameter);
+        cout << "Периметр окружности: " << perimeter1;
+    }
+    else {
+        cout << "Повторите снова";
+    }
+    return 0;
+}
+
+
