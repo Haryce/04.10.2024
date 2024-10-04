fa53e410f5937a003729408e1a3549d9d5938088
Haryce <148765923+Haryce@users.noreply.github.com>
2024-10-04 08:01:23 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int sec;
+    cout << "Введите количество секунд, прошедших с начала дня: ";
+    cin >> sec;
+    int total_seconds_in_day = 86400; // 24 * 60 * 60
+    int seconds_left = total_seconds_in_day - sec;
+    int hours_left = seconds_left / 3600;
+    int minutes_left = (seconds_left % 3600) / 60;
+    int seconds_left_final = seconds_left % 60;
+
+    cout << "До полуночи осталось " << hours_left << " часов, " << minutes_left << " минут, " << seconds_left_final << " секунд.\n";
+
+    return 0;
+}
